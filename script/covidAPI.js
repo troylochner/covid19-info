7936a2fb9fe8384be4f7ca4d1a4bc5942a6a4f93
troylochner <troy.lochner@gmail.com>
2020-09-14 21:29:32 -0400

    splitting js functions into two js files

---
+++ script/covidAPI.js
@@ -0,0 +1,39 @@
+var baseURL = "https://api.covid19api.com/"
+
+function getCountries(){
+    var settings = {
+        "url": baseURL + "countries",
+        "method": "GET",
+        "timeout": 0,
+      }; 
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
+};
+
+function getCountryInfo(slug){
+  var startDate = '2020-09-01';
+  var endDate = '2020-09-14';
+  //var slug = "united-states"
+
+  var settings = {
+    "url": baseURL + "total/country/" + slug + "?from=" + startDate + "&to=" + endDate ,
+    "method": "GET",
+    "timeout": 0,
+    success: function (data) {
+      countryData = data;
+      //LOOP THROUGH THE DAYS
+      for (i=0;i<countryData.length;i++){
+        console.log(countryData[i].Date);
+      }           
+      console.log("getCountryInfo -> countryData", countryData)
+  },
+  error: function (ex) {
+      alert(ex.data);
+  }
+  };
+  
+  $.ajax(settings).done(function (response) {
+    //console.log(response);
+  });
+}
