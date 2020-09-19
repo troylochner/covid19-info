d03c6614483ccb68cb0bd2a71911a0e998fead3e
Brennan Doehring <brennandoehringd@outlook.com>
2020-09-19 12:27:34 -0400

    Restyled website UI.

--- script/covidAPI.js
+++ script/covidAPI.js
@@ -1,11 +1,25 @@
 var baseURL = "https://api.covid19api.com/"
-  var confirmCase = $("#totalConfirmed");
-  var confirmDeath = $("#totalDeaths");
-  var totalRecovered = $("#totalRecovered");
+var confirmCase = $("#totalConfirmed");
+var confirmDeath = $("#totalDeaths");
+var totalRecovered = $("#totalRecovered");
+var countryArray;
+var countryAutoComplete;
+var fullSummary;
+
+//var tabDiv = $("<div>").attr("id","countryTableDiv");
+//var main = $("<main>")
+//tabDiv.appendTo(main);
 
 function init() {
+  
   getSummary();
+  //makeCountryIndex();
+  //getCountries();
 }
+
+function makeTableDiv(){
+  
+};
 
 function getSummary() {
 
@@ -23,7 +37,9 @@ function getSummary() {
     confirmCase.text(response.Global.TotalConfirmed);
     confirmDeath.html(response.Global.TotalDeaths);
     totalRecovered.html(response.Global.TotalRecovered);
+    fullSummary = response;
     //confirm('Did this function');
+    makeCountryIndex();
 
   });
 }
@@ -35,12 +51,169 @@ function getCountries() {
     "timeout": 0,
   };
   $.ajax(settings).done(function (response) {
-    console.log(response);
+    countryArray = response;
+    createAutoComplete(countryArray);
   });
 };
 
+function createAutoComplete(countryArray) {
+  var x = [];
+  for (i = 0; i < countryArray.length; i++) {
+    x.push(countryArray[i].Country)
+  }
+  countryAutoComplete = JSON.stringify(x)
+}
+
+function sendItem(val) {
+  console.log(val);
+}
+
+//BUILDING IN COUNTRY AUTOCOMPLETE - USING ONLY 3 COUNTRIES AS MY START POINT
+$(function () {
+  $('input.autocomplete').autocomplete({
+    data: {
+      "mexico": null,
+      "canada": null,
+      "united-states": null,
+      "austria": null,
+      "yemen": null,
+    },
+    onAutocomplete: function (txt) {
+      //UPON AUTOCOMPLETE - CALL FUNCTION
+      //sendItem(txt);
+      getCountryInfo(txt);
+    },
+    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+  });
+
+});
 
 
+
+//MAKE COUNTRY INDEX TABLE:
+function makeCountryIndex() {
+
+  //GRAB OUR TABLE PLACEMENT DIV
+  var countryTableDiv = $("#countryTableDiv");
+  // create table
+  var $table = $('<table>');
+  $table.attr("id","countryIDX").attr("class","responsive-table centered highlight countryTable");
+
+  // caption
+  $table.append('<caption>Current Case Counts</caption>')
+    // thead
+
+    //ADD SORTING HEADERS :
+    .append('<thead>').children('thead')
+    .append('<tr />').children('tr').append('<th onclick="sortTable(0)">Country</th><th onclick="sortTable(1)">NewConfirmed</th><th onclick="sortTable(2)">TotalConfirmed</th><th onclick="sortTable(3)">NewDeaths</th><th onclick="sortTable(4)">TotalDeaths</th><th onclick="sortTable(5)">NewRecovered</th><th onclick="sortTable(6)">TotalRecovered</th>');
+
+  //tbody
+  var $tbody = $table.append('<tbody />').children('tbody');
+
+  // PLACE IN A FOR EACH LOOP
+  for (i = 0; i < fullSummary.Countries.length; i++) {
+    $tbody.append('<tr />').children('tr:last')
+      .append("<td><a>" + fullSummary.Countries[i].Country + "<a/ data-id=" + fullSummary.Countries[i].Slug + "></td>")
+
+      .append("<td>" + parseFloat(fullSummary.Countries[i].NewConfirmed)+ "</td>")
+      .append("<td>" + parseFloat(fullSummary.Countries[i].TotalConfirmed) + "</td>")
+      .append("<td>" + parseFloat(fullSummary.Countries[i].NewDeaths) + "</td>")
+      .append("<td>" + parseFloat(fullSummary.Countries[i].TotalDeaths) + "</td>")
+      .append("<td>" + parseFloat(fullSummary.Countries[i].NewRecovered) + "</td>")
+      .append("<td>" + parseFloat(fullSummary.Countries[i].TotalRecovered) + "</td>");
+
+  }
+  //LAST STEP
+  $table.appendTo(countryTableDiv);
+
+}
+
+
+//SORT TABLE FUNCTION DIRECT FROM w3:
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("countryIDX");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+
+
+
+//MAKE A TABLE
+function makeTable(x) {
+  //GRAB OUR TABLE PLACEMENT DIV
+  var countryTableDiv = $("#countryTableDiv");
+  // create table
+  var $table = $('<table>');
+  // caption
+  $table.append('<caption>[REPLACE WITH COUNTRY NAME HERE]s</caption>')
+    // thead
+    .append('<thead>').children('thead')
+    .append('<tr />').children('tr').append('<th>Date</th><th>Active</th><th>Confirmed</th><th>Recovered</th>');
+
+  //tbody
+  var $tbody = $table.append('<tbody />').children('tbody');
+
+  // add row
+  $tbody.append('<tr />').children('tr:last')
+    .append("<td>val</td>")
+    .append("<td>val</td>")
+    .append("<td>val</td>")
+    .append("<td>val</td>");
+
+
+  // add table to dom
+  $table.appendTo(countryTableDiv);
+
+}
 
 function getCountryInfo(slug) {
   var startDate = '2020-09-01';
